c75a9ad2cdc3906844b4b46585eeb64c28f4d3d6
Maitri2302 <singhmaitri2323@gmail.com>
2026-03-01 06:57:05 +0000

    numbers and math

---
+++ 01_basics/number.js
@@ -0,0 +1,23 @@
+// const score =400;
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toPrecision(2))
+// const price = 1000000
+// console.log(price.toLocaleString('en-IN'))
+
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(5.7))
+// console.log(Math.ceil(2.3))
+// console.log(Math.min(2,6,8,1))
+// console.log(Math.max(1,2,3,4,5))
+
+console.log(Math.random())
+console.log(Math.floor((Math.random()*10)+1)) //random number between 1 to 10
+
+const min=10
+const max =20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
