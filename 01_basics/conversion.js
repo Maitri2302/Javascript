4e001f3ff3adc515183d756e2d2643aa60f300a0
Maitri2302 <singhmaitri2323@gmail.com>
2026-03-01 05:26:27 +0000

    Type conversion

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -7,3 +7,9 @@ console.log(typeof(scoreInNum))
 let isLoggedIn="Yes";
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
+
+let someNumber= 0;
+let booleanSomeNumber = Boolean(someNumber)
+console.log(booleanSomeNumber)
+console.log(typeof(someNumber))
+console.log(typeof(booleanSomeNumber))
