d7e05300e3e2746e77639145c743a0cb1a733330
Maitri2302 <singhmaitri2323@gmail.com>
2026-03-30 11:01:41 +0000

    scopes

---
+++ 04_scope/scope.js
@@ -0,0 +1,12 @@
+if(true){
+    let a=10
+    const b=8
+    var c=6
+}
+// console.log(a) let is scope dependent
+// console.log(b) so is const
+// console.log(c)  it is not scope dependent and thus is not used 
+
+function one(){
+    co
+}
